29e4a5bd522ec27668ecf9723b37b3e1a87a6332
maayan <maayansh.ms@gmail.com>
2022-05-25 16:48:05 +0300

    try

---
+++ SermoAPI/Models/Ratings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Data;
+
+namespace Sermo_WAPI_Trial2
+{
+	public class Ratings
+	{
+		[Key]
+
+		public int Id { get; set; }
+		public string? Author { get; set; }
+		public string? Content { get; set; }
+
+		public List<Ratings> getRatings()
+		{
+			using (var db = new WebApplication1Context())
+			{
+				var RateList = db.Ratings.ToList();
+				return RateList;
+
+			}
+		}
+
+	}
+}
+

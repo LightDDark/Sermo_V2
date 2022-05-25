d21c723c3c2181f5d5e656b03c4a58136eafd7c1
MaayanSha <92812706+MaayanSha@users.noreply.github.com>
2022-05-25 15:47:48 +0300

    upload1

---
+++ Entries/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sermo_WAPI_Trial2
+{
+    public class User
+    {
+        [Key]
+        public string? Name { get; set; }
+        [Required]
+        public string? Nickname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        public List<User>? Contacts { get; set; }
+        public List<Log>? Logs { get; set; } 
+    }
+}

443c27f383e5f600cd9dc50957fb56f87c397faf
maayan <maayansh.ms@gmail.com>
2022-05-30 22:52:04 +0300

    added date and search ratings

---
+++ SermoAPP/Views/Rating/search.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Repository;
+using Domain.Rate;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SermoAPP.Views.Rating
+{
+	public class indexR
+	{
+		private readonly WebApplication1Context _context;
+
+
+
+		public indexR(WebApplication1Context context)
+		{
+			_context = context;
+		}
+		public IList<Ratings> ratings { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string SearchString { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			var movies = from m in _context.Ratings
+						 select m;
+			if (!string.IsNullOrEmpty(SearchString))
+			{
+				movies = movies.Where(s => s.Content.Contains(SearchString));
+			}
+
+			ratings = await _context.Ratings.ToListAsync();
+		}
+	}
+}
+
+
